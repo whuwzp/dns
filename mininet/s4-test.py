81b7f6baf98e94ae9687e0d7610dc26a8fbfffcf
whuwzp <whuwzp@foxmail.com>
2017-04-14 16:05:47 +0800

    vbox-based

---
+++ mininet/s4-test.py
@@ -0,0 +1,27 @@
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+
+    def __init__( self ):
+
+        # initilaize topology   
+        Topo.__init__( self )
+
+        # add hosts and switches
+        s0 = self.addSwitch( 's0' )       
+        s1 = self.addSwitch( 's1' )       
+        s2 = self.addSwitch( 's2' )       
+        s3 = self.addSwitch( 's3' )       
+
+
+
+
+        # add links
+
+        self.addLink(s0,s1)
+        self.addLink(s0,s2)
+        self.addLink(s0,s3)
+
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
--- query.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-import sys,socket
-result=socket.getaddrinfo(sys,argv[1],None)
-print result[0][4]
